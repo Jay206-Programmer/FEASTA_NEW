17a1f3214571bb86e73d543a91cccb0f7db25983
Jay Shukla <jayshukla0034@gmail.com>
2021-04-02 03:09:01 +0530

    Allowed Cors origins

--- Feasta/settings.py
+++ Feasta/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,9 +43,11 @@ INSTALLED_APPS = [
     'rest_framework',
     'common',
     'authentication',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -53,6 +57,11 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS =[
+    'http://localhost:3000',
+    ]
+
 
 ROOT_URLCONF = 'Feasta.urls'
 
