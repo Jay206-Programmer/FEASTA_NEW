d3dc0e5de707b0862d0ceb17ef105672651e05d0
Jay Shukla <jshukla@infosenseglobal.net>
2021-04-05 03:57:07 +0530

    Some changes

--- authentication/urls.py
+++ authentication/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     #URL For User Login
     path('auth/login/',UserLoginClass.as_view()),
     path('auth/regestration/',UserRegestrationClass.as_view()),
+    path('auth/admin_regestration/',AdminRegestrationClass.as_view()),
     path('verify/<str:unique_id>',verify_user),
     path('auth/get_login_status/',LoginStatusClass.as_view()),
 ]
